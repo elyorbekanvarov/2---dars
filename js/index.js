726d455953ae635d82e31a401abc0224269cbdb2
elyorbekanvarov <e0275king@gmail.com>
2024-07-17 10:35:35 +0500

    2-dars

---
+++ js/index.js
@@ -0,0 +1,256 @@
+// 1 - masala
+
+// let num = + prompt('Sonni kiriting');
+// if (num > 0) {
+//     num++
+// }
+// console.log(num);
+
+// 2 - masala
+
+// let num = + prompt('Sonni kiriting');
+// if (num > 0) {
+//     num++;
+// } else {
+//     num -= 2;
+// }
+// console.log(num);
+
+// 3 - masala
+
+// function num(son) {
+//     if (son > 0) {
+//         return son + 1;
+//     } else if (son < 0) {
+//         return son - 2;
+//     } else {
+//         return 10;
+//     }
+// }
+// let berilganSon = 5;
+// let res = num(son);
+// console.log(res);
+
+// 4 - masala
+
+// let num1 = + prompt("Birinchi sonni kiritng");
+// let num2 = + prompt("Ikkinchi sonni kiriting");
+// let num3 = + prompt("Uchinchi sonni kiriting");
+// let counter = 0;
+// if (num1 > 0) {
+//     counter++
+// } else if (num2 > 0) {
+//     counter++
+// } else (num3 > 0 &); {
+//     counter++
+// }
+// console.log(counter);
+
+// 5 - masala
+
+// let num1 = + prompt("Birinchi sonni kiritng");
+// let num2 = + prompt("Ikkinchi sonni kiriting");
+// let num3 = + prompt("Uchinchi sonni kiriting");
+// let counter = 0;
+// if (num1 > 0 && num1 % 2 == 1) {
+//     counter++
+// } else if (num2 > 0 && num2 % 2 == 1) {
+//     counter++
+// } else (num3 > 0 && num3 % 2 == 1); {
+//     counter++
+// }
+// console.log(counter);
+
+// 6 - masala
+
+// let num1 = + prompt("Birinchi sonni kiriting");
+// let num2 = + prompt("Ikkinchi sonni kiriting");
+// let max;
+// if (num1 > num2) {
+//     max = num1;
+// } else (num2 > num1); {
+//     max = num2;
+// }
+// console.log(max);
+
+// 8 - masala
+
+// let num1 = + prompt("Birinchi sonni kiriting");
+// let num2 = + prompt("Ikkinchi sonni kiriting");
+// let max;
+// let min;
+// if (num2 > num1 && num2 < num1) {
+//     max = num1
+//     min = num2
+// } else (num1 > num2 && num1 < num2); {
+//     max = num2
+//     min = num1
+// }
+// console.log(max,min);
+
+// 9 - masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let min;
+// let max;
+// if (B > A && B < A) {
+//     min = A
+//     max = B
+// } else (A > B && A < B); {
+//     min = B
+//     max = A
+// }
+// console.log(min,max);
+
+// 10 - masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let res;
+// if (A == B) {
+//     res = A + B
+// } else (A = B); {
+//     res = 0
+// }
+// console.log(res);
+
+// 12 - masala
+
+// let num1 = + prompt("Birinchi sonni kiriting");
+// let num2 = + prompt('Ikkinchi sonni kiriting');
+// let num3 = + prompt(`Uchinchi sonni kiriting`);
+// let min;
+// if (num1 < num2) {
+//     min = num1
+// } else if(num2 < num3) {
+//     min = num2
+// } else (num3 < num1); {
+//     min = num3
+// }
+// console.log(min);
+
+// 14 - masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let C = + prompt("C sonini kiriting")
+// let min;
+// let max;
+// if (A > B) {
+//     min = A
+//     max = B
+// } else if (B < C) {
+//     min = B
+//     max = C
+// } else (C < A); {
+//     min = C
+//     max = A
+// }
+// console.log(min, max);
+
+// Boolean 1
+
+// let A = + prompt(`A sonini kiriting`);
+// let res;
+// if (A > 0) {
+//     res = "Musbat"
+// } else {
+//     res = "Musbat emas"
+// }
+// console.log(res);
+
+// Boolean 2
+
+// let a = + prompt('a sonini kiriting');
+// let result;
+// if (a % 2 == 1) {
+//     result = "Toq son"
+// } else {
+//     result = 'Toq son emas'
+// }
+// console.log(result);
+
+// Boolean 3
+
+// let a = + prompt('a sonini kiriting');
+// let result;
+// if (a % 2 == 0) {
+//     result = "Juft son"
+// } else {
+//     result = 'Juft son emas'
+// }
+// console.log(result);
+
+// Boolean 4
+
+// let A = + prompt('A sonini kiriting');
+// let B = + prompt("B sonini kiriting");
+// let natija;
+// if (A > 2 && B <= 3) {
+//     natija = "Rost"
+// } else {
+//     natija = 'Yolgon'
+// }
+// console.log(natija);
+
+// Boolean 5
+
+// let A = + prompt('A sonini kiriting');
+// let B = + prompt("B sonini kiriting");
+// let natija;
+// if (A >= 0 && B < 2) {
+//     natija = "Rost"
+// } else {
+//     natija = 'Yolgon'
+// }
+// console.log(natija);
+
+// Boolean 6
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let C = + prompt("C sonini kiriting");
+// let res;
+// if (A <= B <= C) {
+//     res = 'Rost'
+// } else {
+//     res = "Yolgon"
+// }
+// console.log(res);
+
+// Boolean 8
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let res;
+// if (A % 2 == 1 && B % 2 == 1) {
+//     res = "Rost"
+// } else {
+//     res = 'Yolgon'
+// }
+// console.log(res);
+
+// Boolean 9
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let res;
+// if (A % 2 == 1 !! B % 2 == 1) {
+//     res = "Rost"
+// } else {
+//     res = 'Yolgon'
+// }
+// console.log(res);
+
+// Boolean 10
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let res;
+// if (A % 2 == 1 || B % 2 == 1) {
+//     res = "Rost"
+// } else {
+//     res = 'Yolgon'
+// }
+// console.log(res);
